e08b538dc985d37862e628d2f6b7e522bac74635
acnesiac <acnesiac@hotmail.com>
2019-03-04 17:06:49 -0800

    Reference to  https://github.com/claudiajs/example-projects#web-api

--- javastructurespatternsstreams/datastructure/src/main/java/collection/com/HashMapClass.java
+++ javastructurespatternsstreams/datastructure/src/main/java/collection/com/HashMapClass.java
@@ -1,7 +1,9 @@
 package collection.com;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class HashMapClass {
 	 public static void main(String args[]){  
@@ -15,5 +17,48 @@ public class HashMapClass {
 		for(Map.Entry m:hm.entrySet()){  
 		   System.out.println(m.getKey()+" "+m.getValue());  
 		  }  
-		 } 
+
+
+		map(IntStream.of(new int[]{1,2,3,4,5,6}));
+		max(IntStream.of(new int[]{1,2,3,4,5,6}));
+		max(IntStream.of(new int[]{1,2,3,4,5,}).boxed().collect(Collectors.toList()).stream());
+
+		//filter(IntStream.of(new int[]{1,2,3,4,5,}).boxed().collect(Collectors.toList()).stream());
+
+	}
+
+	private static void map(IntStream of) {
+		IntStream map = of.map(n -> n + 2);
+		map.forEach(System.out::print);
+	}
+
+	private static int max(Stream<Integer> stream) {
+		Optional<Integer> max = stream.max( Integer::compare);
+		return max.get().intValue();
+	}
+
+	private static OptionalInt max(IntStream of) {
+		return of.max();
+	}
+
+	private static Integer[] filter(Stream<Integer> stream) {
+		Stream<Integer> filter = stream.filter(n -> n<3);
+		return (Integer[]) filter.toArray();
+	}
+
+	private static double average(Stream<Integer> stream) {
+		return 0;
+	}
+
+	private static Long count(Stream<Integer> of) {
+		return null;
+	}
+
+	private static Long count(IntStream of) {
+		return of.count();
+	}
+
+	private static OptionalDouble average(IntStream stream) {
+		return stream.average();
+	}
 }
