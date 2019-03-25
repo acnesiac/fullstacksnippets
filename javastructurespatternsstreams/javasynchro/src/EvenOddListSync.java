4624493f07098b76ea5cd76c212ee6b509c13ad8
acnesiac <acnesiac@hotmail.com>
2019-03-24 18:34:39 -0700

    counter

--- javastructurespatternsstreams/javasynchro/src/EvenOddListSync.java
+++ javastructurespatternsstreams/javasynchro/src/EvenOddListSync.java
@@ -3,29 +3,28 @@ public class EvenOddListSync {
 
     public static void main(String... args) {
         Printer print = new Printer();
-        Thread t1 = new Thread(new TaskEvenOdd(print, 10, false), "Odd");
-        Thread t2 = new Thread(new TaskEvenOdd(print, 10, true), "Even");
+        Thread t1 = new Thread(new TaskEvenOdd(print, false), "Odd");
+        Thread t2 = new Thread(new TaskEvenOdd(print, true), "Even");
         t1.start();
         t2.start();
     }
 }
 
 class TaskEvenOdd implements Runnable {
-    private final int max;
     private final Printer print;
     private final boolean isEvenNumber;
 
-    TaskEvenOdd(Printer print, int max, boolean isEvenNumber) {
+    TaskEvenOdd(Printer print,  boolean isEvenNumber) {
         this.print = print;
-        this.max = max;
+
         this.isEvenNumber = isEvenNumber;
     }
 
     @Override
     public void run() {
         int number = isEvenNumber ? 2 : 1;
-        while (number <= max) {
-            if (isEvenNumber) {
+        while (number <= 10) {
+            if (number %2 == 0) {
                 print.printEven(number);
             } else {
                 print.printOdd(number);
