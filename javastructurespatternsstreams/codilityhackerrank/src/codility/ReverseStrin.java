2ddba56fc501fa7994a4be59fdd0409d519ceb3a
acnesiac <acnesiac@hotmail.com>
2019-03-30 21:22:33 -0700

    reverse

--- javastructurespatternsstreams/codilityhackerrank/src/codility/ReverseStrin.java
+++ javastructurespatternsstreams/codilityhackerrank/src/codility/ReverseStrin.java
@@ -3,9 +3,14 @@ package codility;
 public class ReverseStrin {
     public static void main (String args[]){
         char[] arr = new char[]{'a','b', 'c','d'};
-
-        for (){
-
+        char[] res = new char[arr.length];
+        int i = 0;
+        while(i < arr.length ){
+            res[ arr.length-i -1 ] = arr[i];
+            i++;
         }
+
+        System.out.println(res);
+
     }
 }
