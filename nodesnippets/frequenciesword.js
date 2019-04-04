942c4d9b426fcc4f9442e254ace6be6967b7dc94
acnesiac <acnesiac@hotmail.com>
2019-04-03 21:45:58 -0700

    Reference to  https://github.com/claudiajs/example-projects#web-api

--- nodesnippets/frequenciesword.js
+++ nodesnippets/frequenciesword.js
@@ -6,7 +6,8 @@ const unordered = {
 
 const ordered = [];
 
-var res =Object.keys(unordered).sort(function (a,b){return b-a;}).forEach(function (key,index){  
+var res =Object.keys(unordered).sort(
+	function (a,b){return b-a;}).forEach(function (key,index){
 			
 			ordered [key] = unordered[key];
 
