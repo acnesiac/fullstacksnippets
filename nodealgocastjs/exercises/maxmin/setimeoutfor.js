d67008892536300069e8ff5eba6e56964cb9cdfe
acnesiac <acnesiac@hotmail.com>
2019-04-06 12:22:00 -0700

    promises

---
+++ nodealgocastjs/exercises/maxmin/setimeoutfor.js
@@ -0,0 +1,16 @@
+
+(function x(){
+	var d = 10;
+	console.log("a=",d);
+	for(var i = )  
+	setTimeout(function(){
+				console.log("b=",d); 
+				d=15;
+       },1000);
+		d=12
+	console.log("c=",d);  
+})();
+
+
+
+
