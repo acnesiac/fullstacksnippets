2c958134bed89ee9c711ab6d829f17e6f2a5b7b5
acnesiac <acnesiac@hotmail.com>
2019-04-06 18:28:07 -0700

    maxmin

--- nodealgocastjs/exercises/maxmin/maxmin.js
+++ nodealgocastjs/exercises/maxmin/maxmin.js
@@ -3,9 +3,7 @@ var arr = [9,4,7,2];
 const chunkedArr = [];
 let i = 0;
 let len = 2;
-
 arr = arr.sort();
-
 var min = Math.min.apply(null,arr);
 while (i < arr.length) {
 	 var sliced = arr.slice(i, i + len);
