aa1bcc1fa3ea975cd79b50517e4f1d420cb7457e
acnesiac <acnesiac@hotmail.com>
2019-04-16 20:04:20 -0700

    cognixant

---
+++ cognizanttest/Movies-Manager-Services-Java/src/main/java/com/cognizant/devtest/moviesmanager/MoviesManagerApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.devtest.moviesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesManagerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("");
+		SpringApplication.run(MoviesManagerApplication.class, args);
+	}
+
+}
+
