773bd76ba31a10aac671351be46afcc987855302
acnesiac <acnesiac@hotmail.com>
2019-05-22 11:12:28 -0700

    frequency

---
+++ javastructurespatternsstreams/streams/src/streams/Palindrome.java
@@ -0,0 +1,11 @@
+package streams;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("helloworld");
+        StringBuffer reverse = sb.reverse();
+        System.out.println(reverse);
+
+    }
+
+}
