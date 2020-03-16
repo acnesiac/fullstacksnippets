c1a43ec22667bf6a1af649419cdc378cdf309119
acnesiac <acnesiac@hotmail.com>
2020-03-16 17:48:48 -0400

    Mapfilter

--- nodesnippets/frequenciesword/frequenciesword.test.js
+++ nodesnippets/frequenciesword/frequenciesword.test.js
@@ -1,5 +1,5 @@
 const frequenciesword = require('./frequenciesword');
-console.log( frequenciesword());
+console.log( frequenciesword() );
 
 test('check array', () => {
   expect(f.lenght).toHaveLength(3);
