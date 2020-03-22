a9192d2ebb428c3b9fb606e405144406cab55565
acnesiac <acnesiac@hotmail.com>
2020-03-22 09:41:01 -0400

    news frontends

---
+++ frontendsimplevanilla/test/spec/test.js
@@ -0,0 +1,12 @@
+
+(function () {
+  'use strict';
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+    });
+  });
+});
+
+})();
